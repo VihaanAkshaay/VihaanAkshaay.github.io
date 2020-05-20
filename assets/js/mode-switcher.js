5b0fe68fccd922d2c0d1500616595b0ece5829ba
Vihaan Akshaay <akswins001@gmail.com>
2020-05-20 21:29:20 +0530

    Some more breaking in

--- assets/js/mode-switcher.js
+++ assets/js/mode-switcher.js
@@ -1,16 +1,17 @@
 let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
 let theme = sessionStorage.getItem('theme');
+sessionStorage.setItem('theme','dark');
 
 function prefersColorTest(systemInitiatedDark) {
-  if (systemInitiatedDark.matches) {
-	  document.documentElement.setAttribute('data-theme', 'dark');
-	   // document.getElementById("theme-toggle").innerHTML = "Light Mode";
-   	sessionStorage.setItem('theme', '');
-  } else {
-	  document.documentElement.setAttribute('data-theme', 'light');
-    // document.getElementById("theme-toggle").innerHTML = "Dark Mode";
-    sessionStorage.setItem('theme', '');
-  }
+	if (systemInitiatedDark.matches) {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
+		sessionStorage.setItem('theme', 'dark');
+	} else {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+		sessionStorage.setItem('theme', 'dark');
+	}
 }
 systemInitiatedDark.addListener(prefersColorTest);
 
@@ -21,7 +22,7 @@ function modeSwitcher() {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
 		// document.getElementById("theme-toggle").innerHTML = "Dark Mode";
-	}	else if (theme === "light") {
+	} else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
 		// document.getElementById("theme-toggle").innerHTML = "Light Mode";
